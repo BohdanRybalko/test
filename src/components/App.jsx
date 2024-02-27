9de2e8174ac6d654e0eb54c13d1344e01c9dd2a5
BohdanRybalko <147001546+BohdanRybalko@users.noreply.github.com>
2024-02-27 15:20:26 +0200

    save back

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-// App.jsx
-
 import React, { useState, useEffect } from 'react';
 import { getUsers } from '../services/UserService';
 import UserCard from './UserCard';
@@ -8,26 +6,18 @@ import './App.css';
 export const App = () => {
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     const fetchUsers = async () => {
-      try {
-        const { users: userData, totalPages: total } = await getUsers(currentPage);
-        setTotalPages(total);
-        setUsers((prevUsers) => [...new Set([...prevUsers, ...userData])]);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-      }
+      const data = await getUsers(currentPage);
+      setUsers((prevUsers) => [...prevUsers, ...data]);
     };
 
     fetchUsers();
   }, [currentPage]);
 
   const handleLoadMore = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage((prevPage) => prevPage + 1);
-    }
+    setCurrentPage((prevPage) => prevPage + 1);
   };
 
   return (
@@ -35,11 +25,9 @@ export const App = () => {
       {users.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
-      {currentPage < totalPages && (
-        <button onClick={handleLoadMore} className="load-more-btn">
-          Load More
-        </button>
-      )}
+      <button onClick={handleLoadMore} className="load-more-btn">
+        Load More
+      </button>
     </div>
   );
 };
--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,11 +1,7 @@
 const API_BASE_URL = 'https://65ddc801dccfcd562f557200.mockapi.io/';
 
-export const getUsers = async (page = 1, limit = 3) => {
-  const response = await fetch(`${API_BASE_URL}/users?page=${page}&limit=${limit}`);
+export const getUsers = async (page = 1) => {
+  const response = await fetch(`${API_BASE_URL}/users?page=${page}&limit=3`);
   const data = await response.json();
-  
-  return {
-    users: data, 
-    totalPages: data.totalPages, 
-  };
+  return data;
 };
